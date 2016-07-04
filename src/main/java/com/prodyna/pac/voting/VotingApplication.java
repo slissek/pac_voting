bf471c3fe0dd582b046cd38a8fdc8571bb7f1cbf
Sven Lissek <sven.lissek@prodyna.com>
2016-07-04 14:03:49 +0200

    Added HazlecastAutoConfiguration to SpringBootApplication

--- src/main/java/com/prodyna/pac/voting/VotingApplication.java
+++ src/main/java/com/prodyna/pac/voting/VotingApplication.java
@@ -1,13 +1,16 @@
 package com.prodyna.pac.voting;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.prodyna.pac.voting.config.ApplicationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ ApplicationProperties.class })
+@EnableAutoConfiguration(exclude = { HazelcastAutoConfiguration.class })
 public class VotingApplication
 {
     public static void main(final String[] args)
