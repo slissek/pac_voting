330f79c60ab54a4c166ef9a03d7256fc9e7225c5
Sven Lissek <sven.lissek@prodyna.com>
2016-07-26 08:11:45 +0200

    Replaced by Interface

--- src/main/java/com/prodyna/pac/voting/config/Constants.java
+++ src/main/java/com/prodyna/pac/voting/config/Constants.java
@@ -3,18 +3,14 @@ package com.prodyna.pac.voting.config;
 /**
  * Application constants.
  */
-public final class Constants
+public interface Constants
 {
     // Spring profile for development and production
-    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
-    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    String SPRING_PROFILE_PRODUCTION = "prod";
 
     // Spring profile used to disable swagger
-    public static final String SPRING_PROFILE_NO_SWAGGER = "no-swagger";
+    String SPRING_PROFILE_NO_SWAGGER = "no-swagger";
 
-    public static final String SYSTEM_ACCOUNT = "system";
-
-    private Constants()
-    {
-    }
+    String SYSTEM_ACCOUNT = "system";
 }
